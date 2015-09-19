3301b03df5fb7f690b16b5c65ec26e7763c41a52
tonysaur05 <tonysaur@gmail.com>
2015-09-19 16:24:26 -0500

    Added card.cpp and tony_test.cpp
    completed card.cpp and used tony_test.cpp to test all the functions

---
+++ card.cpp
@@ -0,0 +1,76 @@
+
+
+#include "card.h"
+using namespace std;
+
+Card::Card()
+{
+  myRank = 1;
+  mySuit = spades;
+}
+
+Card::Card(int rank, Suit s)
+{
+  myRank = rank;
+  mySuit = s;
+}
+
+string Card::toString() const
+{
+  return rankString(myRank) + suitString(mySuit);
+}
+
+bool Card::sameSuitAs(const Card& c) const
+{
+  if (mySuit == c.mySuit)
+    {
+	  return true;
+	}
+    return false;
+}
+
+int Card::getRank() const
+{
+  return myRank;
+}
+
+string Card::suitString(Suit s) const
+{
+  switch (s)
+  {
+  	case spades:   return "s";
+  	case hearts:   return "h";
+  	case diamonds: return "d";
+  	case clubs:    return "c";
+  }
+}
+
+string Card::rankString(int r) const
+{
+  switch (r)
+  {
+  	case 1:  return "A";
+  	case 11: return "J";
+  	case 12: return "Q";
+  	case 13: return "K";
+  	default: return to_string(myRank);
+  }
+}
+
+bool Card::operator == (const Card& rhs) const
+{
+  if (myRank == rhs.myRank)
+  {
+  	return true;
+  }
+  return false;
+}
+
+bool Card::operator != (const Card& rhs) const
+{
+  if (myRank != rhs.myRank)
+  {
+  	return true;
+  }
+  return false;
+}
---
+++ tony_test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>    // Provides cout and cin
+#include <cstdlib>     // Provides EXIT_SUCCESS
+#include "card.h"
+// #include "player.h"
+// #include "deck.h"
+
+using namespace std;
+
+
+// PROTOTYPES for functions used by this demonstration program:
+//void dealHand(Deck &d, Player &p, int numCards);
+
+
+
+
+int main( )
+{
+  Card card1 = Card();
+  Card card2 = Card(1, Card::hearts);
+  cout << card1.toString() << endl;
+  cout << card2.toString() << endl;
+  cout << "Same suit? " << card1.sameSuitAs(card2) << endl;
+  cout << "Equal Rank? " << (card1 == card2) << endl;
+  cout << "inEqual Rank? " << (card1 != card2) << endl;
+
+
+    // int numCards = 5;
+    
+    // Player p1("Joe");
+    // Player p2("Jane");
+    
+    // Deck d;  //create a deck of cards
+    // d.shuffle();
+    
+    // dealHand(d, p1, numCards);
+    // dealHand(d, p2, numCards);
+       
+    // cout << p1.getName() <<" has : " << p1.showHand() << endl;
+    // cout << p2.getName() <<" has : " << p2.showHand() << endl;
+    
+    
+    // system("PAUSE");
+    
+    return EXIT_SUCCESS;  
+}
+
+
+
+// void dealHand(Deck &d, Player &p, int numCards)
+// {
+//    for (int i=0; i < numCards; i++)
+//       p.addCard(d.dealCard());
+// }
